9b42b6aa15b01c00481c76dbfb9e2a30f5999b20
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-04-19 09:27:43 +0200

    otaceni

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let cards = ['🚗', '🚦', '🚁', '🚃', '⛵️', '⛰', '⛺️', '🏠']
 let pexesoContainer
+let otocene = []
 
 const onLoadFunc = function onLoadFunction() {
   pexesoContainer = document.getElementById('pexeso-container')
@@ -33,13 +34,38 @@ function shuffleArray(array) {
 function addCards(array) {
   array.forEach((card) => {
     let div = document.createElement('div')
-    div.innerText = card
+    div.addEventListener('click', () => {
+      if (otocene.length == 0 || otocene.length == 1) {
+        otocene.push(div)
+        div.classList.add('otoceni')
+        setTimeout(() => (div.innerText = card), 250)
+      }
+      if (otocene.length == 2) reset()
+    })
     pexesoContainer.appendChild(div)
   })
 }
 
 function getRandomNumber(max, min = 0) {
   return Math.floor(Math.random() * (max - min) + min)
+}
+
+function reset() {
+  setTimeout(() => {
+    otocene.forEach((otocena) => {
+      otocena.classList.remove('otoceni')
+      setTimeout(() => {
+        otocena.classList.add('otoceni')
+        setTimeout(() => {
+          otocena.innerText = ''
+        }, 250)
+        setTimeout(() => {
+          otocena.classList.remove('otoceni')
+        }, 500)
+      }, 9)
+    })
+    otocene = []
+  }, 2000)
 }
 
 window.onload = onLoadFunc
